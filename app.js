78c9bb42afae81a3abd93059cce3f1e29c67f845
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-09-29 19:07:28 +0530

    completion

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+
+//Grab input elements
 const principle = document.getElementById("principle");
 const interest = document.getElementById("interest");
 const tenure = document.getElementById("tenure");
 
-const output = document.querySelector(".output");
+//Grab button
 const calculateBtn = document.getElementById("calculate");
 
-
+//Grab output elements
+const output = document.querySelector(".output");
 let emi = document.getElementById("emi")
 let interestpayable = document.getElementById("interestpayable")
 let totalamount = document.getElementById("totalamount")
 
+
+//event listener
 calculateBtn.addEventListener('click',() => {
     if(principle.value > 0 && interest.value > 0 && tenure.value > 0){
+        //principle.value = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(principle.value);
         //console.log(typeof(principle.value));
+        //console.log(principle.innerText);
           let currInt = interestPayable(principle.value,interest.value,tenure.value);
             //console.log(currInt)
           let currTotal = totalAmount(parseInt(principle.value),currInt)
@@ -21,31 +28,39 @@ calculateBtn.addEventListener('click',() => {
           //console.log(monthlyEMI);
 
         output.classList.toggle("hidden");
-
-        emi.innerHTML = monthlyEMI;
-        interestpayable.innerHTML  = currInt;
-        totalamount.innerHTML  = currTotal;
+        //monthlyEMI.toLocaleString('en-IN', { maximumFractionDigits: 0 });
+        //convert number in to india format and store
+        emi.innerHTML = monthlyEMI.toLocaleString('en-IN', { maximumFractionDigits: 0 });
+        interestpayable.innerHTML  = currInt.toLocaleString('en-IN', { maximumFractionDigits: 0 });
+        totalamount.innerHTML  = currTotal.toLocaleString('en-IN', { maximumFractionDigits: 0 });
 
         
 
     }
     else{
+        //sending alert/error message
         alert("Enter all fields with valid numbers")
     }
 
 
 });
+
+//function for calculating EMI
 function EMI(t,time){
     return parseInt((t/(12*time)))
     
 }
 
+
+//function for calculating Interest
 function interestPayable(p,r,t){
     let simpleInterest = (p * r * t)/100;
     return simpleInterest;
 
 }
 
+
+//function for calculating Principle + Interest
 function totalAmount(p,i){
     return p+i;
 
